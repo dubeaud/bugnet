767a88cea6d893ed331598640197a597a81dd555
Davin Dubeau <davin.dubeau@gmail.com>
2012-06-02 14:49:26 -0400

    Added missing file

---
+++ src/BugNET.Common/EmailFormatType.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EmailFormatType.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace BugNET.Common
+{
+    /// <summary>
+    /// Email Format Type
+    /// </summary>
+    public enum EmailFormatType
+    {
+        /// <summary>
+        /// Text
+        /// </summary>
+        Text = 1,
+        /// <summary>
+        /// HTML
+        /// </summary>
+        HTML = 2
+    }
+}
