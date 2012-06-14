951053edc569e1dcd9cd8b42e6e11fb0c39565b1
William Highfield <wrhighfield@gmail.com>
2012-06-13 19:47:23 -0500

    Update/Validate proper permissions on edit project page

--- src/BugNET_WAP/Administration/Projects/EditProject.aspx.cs
+++ src/BugNET_WAP/Administration/Projects/EditProject.aspx.cs
@@ -22,11 +22,15 @@ namespace BugNET.Administration.Projects
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
-		protected void Page_Load(object sender, System.EventArgs e)
+		protected void Page_Load(object sender, EventArgs e)
 		{
-
-            if (!UserManager.HasPermission(ProjectId, Globals.Permission.AdminEditProject.ToString()))
-                Response.Redirect("~/Errors/AccessDenied.aspx");
+            if (!UserManager.IsSuperUser())
+            {
+                if (!UserManager.HasPermission(ProjectId, Globals.Permission.AdminEditProject.ToString()))
+                {
+                    Response.Redirect("~/Errors/AccessDenied.aspx");
+                }   
+            }
 
 			if (!Page.IsPostBack)
 			{
