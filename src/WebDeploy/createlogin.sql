4e125f3ddd4b9199f2fa677f384d09d1aeefa6ae
Davin Dubeau <davin.dubeau@gmail.com>
2014-02-19 20:06:26 -0500

    added web deploy files

---
+++ src/WebDeploy/createlogin.sql
@@ -0,0 +1,11 @@
+/**********************************************************************/
+/* CreateLogin.SQL */
+/* Creates a login */
+/* This scripts runs on master to create login */
+/* Supports SQL Server and SQL AZURE */
+/**********************************************************************/
+
+-- Add here a test to be sure the login does not exist
+-- Create login
+
+CREATE LOGIN PlaceholderForDbUsername WITH PASSWORD = 'PlaceholderForDbUserPassword'
