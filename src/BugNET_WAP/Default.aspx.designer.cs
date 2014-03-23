6119f173259438ed617fcf9d5338b3fcafc6a548
Davin Dubeau <davin.dubeau@gmail.com>
2014-03-23 16:07:51 -0400

    shortened width of quick links drop down
    --HG--
    branch : uienhancements

--- src/BugNET_WAP/Default.aspx.designer.cs
+++ src/BugNET_WAP/Default.aspx.designer.cs
@@ -40,6 +40,60 @@ namespace BugNET {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl BlankSlate;
         
         /// <summary>
+        /// WelcomeLocalize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize WelcomeLocalize;
+        
+        /// <summary>
+        /// StartWorkLocalize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize StartWorkLocalize;
+        
+        /// <summary>
+        /// Step1_Localize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize Step1_Localize;
+        
+        /// <summary>
+        /// Step2_Localize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize Step2_Localize;
+        
+        /// <summary>
+        /// Step3_Localize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize Step3_Localize;
+        
+        /// <summary>
+        /// LearnMoreLocalize control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize LearnMoreLocalize;
+        
+        /// <summary>
         /// UserMessage control.
         /// </summary>
         /// <remarks>
@@ -65,11 +119,5 @@ namespace BugNET {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptProject;
-        
-        protected global::System.Web.UI.WebControls.Localize Step1_Localize;
-        
-        protected global::System.Web.UI.WebControls.Localize Step2_Localize;
-        
-        protected global::System.Web.UI.WebControls.Localize Step3_Localize;
     }
 }
