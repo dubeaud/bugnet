5b1df4ff8f75b3ee1bfd714f2cad92b8612d9311
Davin Dubeau <davin.dubeau@gmail.com>
2014-04-09 20:50:05 -0400

    #182 - fixed get users stored proc to properly get all users even if they do not have any roles.

--- src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
+++ src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
@@ -468,3 +468,29 @@ BEGIN
 	WHERE CustomFieldId = @CustomFieldId AND CustomFieldValue = @OldSelectionValue
 END
 GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[BugNet_User_GetUsersByProjectId]
+	@ProjectId Int,
+	@ExcludeReadonlyUsers bit
+AS
+SELECT DISTINCT U.UserId, U.UserName, FirstName, LastName, DisplayName FROM 
+	Users U
+JOIN BugNet_UserProjects
+	ON U.UserId = BugNet_UserProjects.UserId
+JOIN BugNet_UserProfiles
+	ON U.UserName = BugNet_UserProfiles.UserName
+JOIN  Memberships M 
+	ON U.UserId = M.UserId
+LEFT JOIN BugNet_UserRoles UR
+	ON U.UserId = UR.UserId
+LEFT JOIN BugNet_Roles R
+	ON UR.RoleId = R.RoleId
+WHERE
+	BugNet_UserProjects.ProjectId = @ProjectId 
+	AND M.IsApproved = 1 AND (@ExcludeReadonlyUsers = 0 OR @ExcludeReadonlyUsers = 1 AND R.RoleName != 'Read Only')
+ORDER BY DisplayName ASC
+GO
