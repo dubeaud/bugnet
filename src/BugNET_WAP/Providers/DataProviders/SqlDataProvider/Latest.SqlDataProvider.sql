8d2aaf6d5b976db11466226d5d4a6877d52b11d3
Davin Dubeau <davin.dubeau@gmail.com>
2014-06-01 21:20:49 -0400

    added / removed appsettings in upgrade script

--- src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
+++ src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
@@ -494,3 +494,16 @@ WHERE
 	AND M.IsApproved = 1 AND (@ExcludeReadonlyUsers = 0 OR @ExcludeReadonlyUsers = 1 AND R.RoleName != 'Read Only')
 ORDER BY DisplayName ASC
 GO
+
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('GoogleAuthentication', 'False')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('FacebookAuthentication', 'False')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('FacebookAppId', '')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('FacebookAppSecret', '')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('TwitterAuthentication', 'False')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('TwitterConsumerKey', '')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('TwitterConsumerSecret', '')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('MicrosoftAuthentication', 'False')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('MicrosoftClientId', '')
+INSERT INTO [dbo].[BugNet_HostSettings] ([SettingName], [SettingValue]) VALUES('MicrosoftClientSecret', '')
+DELETE FROM [dbo].[BugNet_HostSettings] WHERE SettingName = 'OpenIdAuthentication'
+GO
