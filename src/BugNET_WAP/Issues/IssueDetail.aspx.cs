a68acc9e667908b71be1cf62c3ffff11731d0d4c
Davin Dubeau <davin.dubeau@gmail.com>
2014-06-04 21:34:17 -0400

    fixed missing web service reference fixed bug loading issue detail page when no defaults are assigned removed user instance param from connection string fixed issue with tabs, commented out the asp.net menu standards js

--- src/BugNET_WAP/Issues/IssueDetail.aspx.cs
+++ src/BugNET_WAP/Issues/IssueDetail.aspx.cs
@@ -214,24 +214,28 @@ namespace BugNET.Issues
                 VoteButton.Visible = false;
                 VotedLabel.Visible = false;
             }
+
             List<DefaultValue> defValues = IssueManager.GetDefaultIssueTypeByProjectId(ProjectId);
             DefaultValue selectedValue = defValues.FirstOrDefault();
 
-            // Visibility Section
-            IssueTypeField.Visible = selectedValue.TypeVisibility;
-            StatusField.Visible = selectedValue.StatusVisibility;
-            PriorityField.Visible = selectedValue.PriorityVisibility;
-            PrivateField.Visible = selectedValue.PrivateVisibility;
-            CategoryField.Visible = selectedValue.CategoryVisibility;
-            DueDateField.Visible = selectedValue.DueDateVisibility;
-            ProgressField.Visible = selectedValue.PercentCompleteVisibility;
-            MilestoneField.Visible = selectedValue.MilestoneVisibility;
-            EstimationField.Visible = selectedValue.EstimationVisibility;
-            ResolutionField.Visible = selectedValue.ResolutionVisibility;
-            AffectedMilestoneField.Visible = selectedValue.AffectedMilestoneVisibility;
-            AssignedToField.Visible = selectedValue.AssignedToVisibility;
-            //chkNotifyAssignedTo.Checked = selectedValue.AssignedToNotify;
-            //chkNotifyOwner.Checked = selectedValue.OwnedByNotify;
+            if (selectedValue != null)
+            {
+                // Visibility Section
+                IssueTypeField.Visible = selectedValue.TypeVisibility;
+                StatusField.Visible = selectedValue.StatusVisibility;
+                PriorityField.Visible = selectedValue.PriorityVisibility;
+                PrivateField.Visible = selectedValue.PrivateVisibility;
+                CategoryField.Visible = selectedValue.CategoryVisibility;
+                DueDateField.Visible = selectedValue.DueDateVisibility;
+                ProgressField.Visible = selectedValue.PercentCompleteVisibility;
+                MilestoneField.Visible = selectedValue.MilestoneVisibility;
+                EstimationField.Visible = selectedValue.EstimationVisibility;
+                ResolutionField.Visible = selectedValue.ResolutionVisibility;
+                AffectedMilestoneField.Visible = selectedValue.AffectedMilestoneVisibility;
+                AssignedToField.Visible = selectedValue.AssignedToVisibility;
+                //chkNotifyAssignedTo.Checked = selectedValue.AssignedToNotify;
+                //chkNotifyOwner.Checked = selectedValue.OwnedByNotify;
+            }
         }
 
         /// <summary>
