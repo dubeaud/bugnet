5570f942d17d0a36614799055f8b17b52ec82192
Davin Dubeau <davin.dubeau@gmail.com>
2014-06-17 09:26:16 -0400

    updated all projects to .net 4.5 changed mail service to use sendmailasync instead of sendasync

--- src/BugNET.BLL/Notifications/SmtpMailDeliveryService.cs
+++ src/BugNET.BLL/Notifications/SmtpMailDeliveryService.cs
@@ -65,7 +65,7 @@ namespace BugNET.BLL.Notifications
 #if(DEBUG)
                 client.Send(message);
 #else
-                client.SendMailAsync(message, null);
+                client.SendMailAsync(message);
 #endif
             }
             catch (Exception)
