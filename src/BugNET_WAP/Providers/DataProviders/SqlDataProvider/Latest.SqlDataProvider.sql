ee134bcfcb68d3455a7608195c4df9e624173b0b
vmasanas <vmasanas@disgrafic.com>
2015-03-22 19:16:20 +0100

    Mailbox enhancements #101 Fix sp BugNet_User_GetUsersByProjectId

--- src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
+++ src/BugNET_WAP/Providers/DataProviders/SqlDataProvider/Latest.SqlDataProvider.sql
@@ -677,3 +677,28 @@ UPDATE BugNet_ProjectMailBoxes SET
 	CategoryId = @CategoryId
 WHERE ProjectMailboxId = @ProjectMailboxId
 GO
+
+
+ALTER PROCEDURE [dbo].[BugNet_User_GetUsersByProjectId]
+	@ProjectId Int,
+	@ExcludeReadonlyUsers bit
+AS
+SELECT DISTINCT U.UserId, U.UserName, FirstName, LastName, DisplayName, Email FROM 
+	Users U
+JOIN BugNet_UserProjects
+	ON U.UserId = BugNet_UserProjects.UserId
+JOIN BugNet_UserProfiles
+	ON U.UserName = BugNet_UserProfiles.UserName
+JOIN  Memberships M 
+	ON U.UserId = M.UserId
+LEFT JOIN BugNet_UserRoles UR
+	ON U.UserId = UR.UserId 
+LEFT JOIN BugNet_Roles R
+	ON UR.RoleId = R.RoleId AND R.ProjectId = @ProjectId
+WHERE
+	BugNet_UserProjects.ProjectId = @ProjectId 
+	AND M.IsApproved = 1
+	AND (@ExcludeReadonlyUsers = 0 OR @ExcludeReadonlyUsers = 1 AND R.RoleName != 'Read Only')
+ORDER BY DisplayName ASC
+GO
+
