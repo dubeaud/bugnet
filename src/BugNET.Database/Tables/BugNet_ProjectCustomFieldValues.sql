c53b8939d187c328119f8f38fe28d8220f79864a
Patrick Kalkman <pkalkie@gmail.com>
2015-05-30 14:31:24 +0200

    Implements #14 being able to delete a custom field

--- src/BugNET.Database/Tables/BugNet_ProjectCustomFieldValues.sql
+++ src/BugNET.Database/Tables/BugNet_ProjectCustomFieldValues.sql
@@ -2,7 +2,7 @@
     [CustomFieldValueId] INT            IDENTITY (1, 1) NOT NULL,
     [IssueId]            INT            NOT NULL,
     [CustomFieldId]      INT            NOT NULL,
-    [CustomFieldValue]   NVARCHAR (MAX) NOT NULL,
+    [CustomFieldValue]   NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_BugNet_ProjectCustomFieldValues] PRIMARY KEY CLUSTERED ([CustomFieldValueId] ASC),
     CONSTRAINT [FK_BugNet_ProjectCustomFieldValues_BugNet_Issues] FOREIGN KEY ([IssueId]) REFERENCES [dbo].[BugNet_Issues] ([IssueId]) ON DELETE CASCADE,
     CONSTRAINT [FK_BugNet_ProjectCustomFieldValues_BugNet_ProjectCustomFields] FOREIGN KEY ([CustomFieldId]) REFERENCES [dbo].[BugNet_ProjectCustomFields] ([CustomFieldId])
